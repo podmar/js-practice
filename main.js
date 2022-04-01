6307c81983d1251d071d1185ab58b6ddfa4ac7e7
podmar <marta.podziewska@gmail.com>
2022-04-01 17:52:23 +0200

    exercises on variables

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log("starting JS")
+// VARIABLES EX. 1
+console.log("starting JS", "\nand it is working")
+
+// EX. 2
+let myAge = 35;
+console.log(myAge);
+
+// EX. 3
+let juliaAge = 32;
+let ageDif = myAge - juliaAge;
+console.log(ageDif);
+
+// CONDITIONALS EX. 4
+if (myAge > 21) {
+    console.log("You are older than 21.");
+} else {
+    console.log("You are not older than 21.");
+}; 
+
+// EX. 5
+if (juliaAge > myAge) {
+    console.log("Julia is older than you.");
+} else if (juliaAge < myAge) {
+    console.log("Julia is younger than you.");
+} else {
+    console.log("You and Julia are the same age.");
+};
+
+// ARRAYS EX. 1
+let quokkas = ["Marta", "Jess", "Angelina", "Arno", "Alex", ""];
