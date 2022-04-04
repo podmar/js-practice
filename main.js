f9d588864b73715f179aa76f2e8a1f0f570adf72
podmar <marta.podziewska@gmail.com>
2022-04-04 10:51:01 +0200

    sorting and looping over arrays

--- main.js
+++ main.js
@@ -27,4 +27,27 @@ if (juliaAge > myAge) {
 };
 
 // ARRAYS EX. 1
-let quokkas = ["Marta", "Jess", "Angelina", "Arno", "Alex", ""];
+let quokkas = ["Marta", "Jess", "Angelina", "Karolina", "Arno", "Alex", "John", "Ottavia", "Raul"];
+quokkas.sort();
+console.log(quokkas);
+console.log(quokkas[quokkas.length-1]);
+
+for (let quokkaIndex = 0; quokkaIndex < quokkas.length; quokkaIndex++) {
+    console.log(quokkas[quokkaIndex]);
+};
+
+// ARRAYS EX. 2
+let quokkaAge = [27, 28, 29, 30, 31, 32, 33, 34, 35]; 
+
+let num = 0;
+while (num<quokkaAge.length) {
+    if (quokkaAge[num]%2 == 0) {
+        console.log(quokkaAge[num])
+        };
+        num++;
+};
+
+for (n=0; n<quokkaAge.length; n++) {
+    console.log(quokkaAge[n]);
+}; 
+
